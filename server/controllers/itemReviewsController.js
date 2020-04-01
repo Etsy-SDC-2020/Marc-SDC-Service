5c998bf3c8af9bbc7a34b355419bdb57cee0d2bd
Marc L <bigmarco@Marcs-MacBook-Pro.local>
2020-04-01 11:26:37 -0500

    Base Files

---
+++ server/controllers/itemReviewsController.js
@@ -0,0 +1,18 @@
+
+const getItemReviews = (req, res) => {
+
+}
+
+const postItemReview = (req, res) => {
+
+}
+
+const updateItemReview = (req, res) => {
+
+}
+
+const deleteItemRevie = (req, res) => {
+
+}
+
+module.exports = {getItemReviews, postItemReview, updateItemReview, deleteItemRevie};
